4a481d480b18a0137d398115cffe5498a09231a4
StellaMontso <marystella.montso@gmail.com>
2022-06-08 21:39:16 +0200

    0x02. C - Functions, nested loops Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
  * main - Entry point
  *
- * Return: 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return 0;
 }
